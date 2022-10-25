123029ff642f88dbf9a208e06428747e36ad695f
Shreepa Parthaje <shreepa.parthaje@gmail.com>
2022-10-25 02:11:17 -0400

    WIP

--- scripts/solar.js
+++ scripts/solar.js
@@ -1,21 +1,26 @@
 const step_size = 0.05;
+const canvas_width = window.innerWidth * 0.3;
+const canvas_height = window.innerHeight * 0.3
+
+window.scrollBy({top: -document.documentElement.scrollTop})
 
 let previousPosition = window.pageYOffset || document.documentElement.scrollTop;
 const scene = new THREE.Scene({background: 0xffffff});
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera( 85, canvas_width / canvas_height , 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer({ alpha: true });
 
-const window_width = window.innerWidth * 0.3;
-const window_height = window.innerHeight * 0.8
-renderer.setSize( window_width, window_height );
+renderer.setSize( canvas_width, canvas_height );
 
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 2, 2 * window_width / window_height, 2);
+const geometry = new THREE.BoxGeometry( 6, 1.5, 2);
 const material = new THREE.MeshLambertMaterial( { color: 0x0ffff0 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
+
+cube.rotation.x = 2
+cube.rotation.z = -0.5
 
 var light = new THREE.DirectionalLight(0xffffff);
 light.position.set(0, 2, 2);
@@ -46,14 +51,14 @@ window.addEventListener("scroll", (event) => {
     let direction = currentPosition > previousPosition ? 1 : -1;
     // let delta = direction * (step_size);
     console.log(contentHeight)
-    let delta = (currentPosition - previousPosition) / contentHeight;
+    let delta = 3 * (currentPosition - previousPosition) / contentHeight;
 
-    if (cube.rotation.y + delta < 0) 
-        cube.rotation.y = 0;
-    else if (cube.rotation.y + delta > 6.2918) 
-        cube.rotation.y = 6.2918;
-    else
-        cube.rotation.y += delta;
+    // if (cube.rotation.y + delta < 0) 
+    //     cube.rotation.y = 0;
+    // else if (cube.rotation.y + delta > 6.2918) 
+    //     cube.rotation.y = 6.2918;
+    // else
+        cube.rotation.z += delta;
 
     console.log(cube.rotation.y)
     previousPosition = currentPosition;
@@ -62,6 +67,11 @@ window.addEventListener("scroll", (event) => {
 function animate() {
     requestAnimationFrame( animate );
     renderer.render( scene, camera );
+}
+
+function scrollTest() {
+    console.log("HI")
+    window.scrollBy({top: 15})
 }
 
 animate();
