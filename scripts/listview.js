7db06b3fc9cc43f5c5d633d6740b0892e9517c23
Shreepa Parthaje <shreepa.parthaje@gmail.com>
2022-12-30 18:35:46 -0500

    Listview upgrade

--- scripts/listview.js
+++ scripts/listview.js
@@ -1,26 +1,34 @@
 let inView = false;
 
 const slideup = () => {
+    const x = document.getElementById("test2").querySelectorAll("*");
     if (inView) {
         inView = false;
         document.getElementById("listview").animate({
-            transform: `translate(0%, 100vh)`
+            transform: `translate(100vw, 0%)`
         }, { duration: 600, fill: "forwards" });
     
-        document.getElementById("slide-button").animate({
-            transform: `translate(40vw, 90vh)`
-        }, { duration: 600, fill: "forwards" });
-        document.getElementById("slide-button").innerText = "<< expand list view >>"
+
+        document.getElementById("slide-button").innerText = ":= list view"
+        setTimeout(function() {
+            x.forEach((e) => { 
+                if (e.innerText !== "•") e.style.color = "#cc0000";
+            }) 
+        }, 10);
+          
         return;
     }
 
     inView = true;
     document.getElementById("listview").animate({
-        transform: `translate(0%, 17vh)`
+        transform: `translate(0%, 0%)`
     }, { duration: 600, fill: "forwards" });
 
-    document.getElementById("slide-button").animate({
-        transform: `translate(40vw, 14vh)`
-    }, { duration: 600, fill: "forwards" });
+    setTimeout(function() {
+        x.forEach((e) => { 
+            if (e.innerText !== "•") e.style.color = "white";
+        }) 
+    }, 550);
+      
     document.getElementById("slide-button").innerText = "X show gallery view"
 }
