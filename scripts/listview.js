9d60fdf105b5e5f0aaa788f405ae74944cb50fc3
Shreepa Parthaje <shreepa.parthaje@gmail.com>
2022-12-30 19:29:02 -0500

    List view upgrade 2

--- scripts/listview.js
+++ scripts/listview.js
@@ -1,34 +1,48 @@
 let inView = false;
 
 const slideup = () => {
-    const x = document.getElementById("test2").querySelectorAll("*");
+    const x = document.getElementById("nav-section").querySelectorAll("*");
     if (inView) {
         inView = false;
-        document.getElementById("listview").animate({
-            transform: `translate(100vw, 0%)`
-        }, { duration: 600, fill: "forwards" });
-    
+        // document.getElementById("listview").animate({
+        //     transform: `translate(100vw, 0%)`
+        // }, { duration: 600, fill: "forwards" });
+        
+        [...document.getElementsByClassName("list-fill-animation")].forEach(e => {
+            e.animate({
+                transform: `translate(${e.dataset.position})`
+            }, { duration: 600, fill: "forwards" });
+        })
 
         document.getElementById("slide-button").innerText = ":= list view"
+
         setTimeout(function() {
             x.forEach((e) => { 
                 if (e.innerText !== "•") e.style.color = "#cc0000";
             }) 
-        }, 10);
+            document.getElementById("slide-button").style.color = "#555555"
+        }, 550);
           
         return;
     }
 
     inView = true;
-    document.getElementById("listview").animate({
-        transform: `translate(0%, 0%)`
-    }, { duration: 600, fill: "forwards" });
+    // document.getElementById("listview").animate({
+    //     transform: `translate(0%, 0%)`
+    // }, { duration: 600, fill: "forwards" });
 
     setTimeout(function() {
         x.forEach((e) => { 
             if (e.innerText !== "•") e.style.color = "white";
         }) 
-    }, 550);
+        document.getElementById("slide-button").style.color = "#aaaaaa"
+    }, 10);
+
+    [...document.getElementsByClassName("list-fill-animation")].forEach(e => {
+        e.animate({
+            transform: `translate(0%, 0%)`
+        }, { duration: 600, fill: "forwards" });
+    })
       
     document.getElementById("slide-button").innerText = "X show gallery view"
 }
